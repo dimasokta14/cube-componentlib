c79bea264e9ba5734542fc3a25866c7b2145ceae
dimas.OA <dimas.oktaarifani@ccn.com.sg>
2023-06-18 22:41:20 +0700

    add avatar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
 export { default as Button } from "./Button";
 export { default as Icon } from './Icons';
+export { default as Box } from './Box';
+export { default as Avatar } from './Avatar';
+export { default as Title } from './Title';
+export { default as Text } from './Text';
